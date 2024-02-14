79e58e0d4b336159c9aed8ac89baab53833d1f0c
sentack <sentak16@gmail.com>
2024-02-14 16:43:02 +0300

    finished the tutorial....works like a charm

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-import { Configuration, OpenAI} from "openai";
-import dotenv from 'dotenv';
-dotenv.config();
-
-const configuration = new Configuration({
-    apiKey: process.env.OpenAI_API_KEY
-});
-
-const openai = new OpenAIApi(configuration);
+import openai from "./config/open-ai.js";
+import readlineSync from 'readline-sync';
+import colors from 'colors';
 
 async function main(){
-    
-}
+
+    console.log(colors.bold.green('Welcome to the ChatBot Program!'));
+    console.log(colors.bold.green('You can Ask me anything!'));
+
+    const chatHistory = [];
+
+
+    while(true){
+        const userInput = readlineSync.question(colors.yellow('You: '));
+
+        try{
+            const messages = chatHistory.map(([role, content]) => ({role, content}));
+
+            messages.push({role: 'user', content: userInput})
+
+            const chatCompletion = await openai.chat.completions.create({
+                model: "gpt-3.5-turbo",
+                messages: messages,
+              });
+            const completionText = chatCompletion.choices[0].message.content;
+
+            console.log(colors.green('Bot: ') + completionText);
+
+            chatHistory.push(['user', userInput]);
+            chatHistory.push(['assistant', completionText]);
+
+            if (userInput.toLowerCase() == 'exit'){
+                return;
+            }
+
+        }catch(error){
+            console.log(colors.red(error));
+        }
+    }
+    }
+
+main();
